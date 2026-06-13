349b6cdb485f3d555592c3cd96e6bdfe46f41fa8
Avnish Patel <er.avnishpatel023@gmail.com>
2026-06-13 22:22:28 +0530

    Clean up Console.js and add REPL and variable info
    Removed redundant comments and added explanations about REPL and variables.

--- JAVASCRIPT/Part 1/Console.js
+++ JAVASCRIPT/Part 1/Console.js
@@ -1,6 +1,12 @@
 // Using the Console
-// Using the Console
-// Using the Console
-// Using the Console
-// Using the Console
-// Using the Console
+
+Uses REPL
+REPL means Read-Evaluate-Print // loop
+
+What is Variable?
+  A variable is simply the name of a storage location.
+
+  exapmle : age = 23
+
+       where, age --> variable
+              23 --> value
