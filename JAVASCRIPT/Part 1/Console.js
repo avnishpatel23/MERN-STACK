8d1dde9cae11afbcbe887169273449e3219b205c
Avnish Patel <er.avnishpatel023@gmail.com>
2026-07-02 23:38:47 +0530

    Enhance Console.js with JS concepts and examples
    Added explanations for variables, operators, NaN, operator precedence, and keywords in JavaScript.

--- JAVASCRIPT/Part 1/Console.js
+++ JAVASCRIPT/Part 1/Console.js
@@ -3,10 +3,45 @@
 Uses REPL
 REPL means Read-Evaluate-Print // loop
 
-What is Variable?
+ -- > What is Variable?
   A variable is simply the name of a storage location.
 
   exapmle : age = 23
 
        where, age --> variable
               23 --> value
+
+-- > Operators in JS?
+  addition        a+b
+  subtraction     a-b
+  multiplication  a*b
+  division        a/b
+  modulo          a%b
+  power          a**b
+
+-- > NaN (Not a Number) : Tha NaN global Property is a value representing 
+     0/0
+    NaN - 1
+    NaN * 1
+    NaN + NaN
+
+-- > Operator Precedence : 
+      ()
+      **
+      *,/,%
+      +,-
+
+-- > let Keyword : 
+
+  let age = 23;
+  age = age + 1;
+
+-- > const keyword : 
+
+  const year = 2026;
+
+   year = 2027  // Error
+   year = year + 1  // Error
+
+
+
